611987a387388a2d6c57753cff961656403086b2
cintyanadaa <cintyanadaa17@gmail.com>
2020-11-11 12:10:38 +0700

    publish file index

---
+++ Persegi/src/Persegi/Persegi.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Persegi;
+
+/**
+ *
+ * @author Sindy
+ */
+public class Persegi {
+    public static void main (String [] args){
+        int sisi;
+        int hslkeliling;
+        int hslluas;
+        
+        sisi = 11;
+        hslkeliling = 4 * sisi;
+        hslluas = sisi * sisi;
+        
+        System.out.println("Hasil Keliling Persegi = " + hslkeliling);
+        System.out.println("Hasil Luas Persegi " + hslluas);
+        
+    }
+    
+}
